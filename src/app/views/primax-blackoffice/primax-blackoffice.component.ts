4077217528250eba8299b5e9a3f99ada8ba41155
jandro29 <ascolanr@gmail.com>
2023-01-03 11:34:46 -0500

    first commit

--- src/app/views/primax-blackoffice/primax-blackoffice.component.ts
+++ src/app/views/primax-blackoffice/primax-blackoffice.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimaxBlackofficeComponent implements OnInit {
 
+  colorWhite = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  date: number = 2021;
+  industry: string = 'INTELIGENCIA ARTIFICIAL';
+  client: string = 'Primax-Backoffice';
+  styles: string = 'w-[100%] lg:ml-32'
+  display: string = 'hidden'
+  imageTwo: string = 'assets/img/primax-solutionsicono.png'
+  image: string = 'assets/img/primera-img-primax-backoffice-.png';
+  paragraphOne: string = 'Nuestro cliente es una solución SaaS que permite automatizar las comunicaciones outbound usando bots de inteligencia artificial, tenía como objetivo posicionar su marca y realizar investigación en sus usuarios actuales para realizar mejoras en su producto digital.';
+  paragraphTwo: string = 'Pasamos muchas horas con el equipo de Notifai identificando el viaje de cliente, a partir de ahí , identificamos oportunidades y puntos de estrés, que validamos con usuarios con sesiones de recorrido cognitivo, mejorando las funcionalidades del sistema.';
+
+  serviceList: Array<String> = [
+    'DESCUBRIMIENTO DE PRODUCTO',
+    'DISEÑO DE PRODUCTO',
+    'DESARROLLO DE SOFTWARE',
+  ];
+
+  definitionsList: Array<String> = [
+    'SESIÓN DE ALCANCE',
+    'AUDITORÍA UX/UI',
+    'COMPORATAMIENTO DE USUARIOS',
+    'DISEÑO VISUAL',
+    'SESIONES DE EQUIPO',
+    'ESTRUCTURA DEL SITIO WEB',
+  ]
+
+  animatedTitle: Array<{aos: number, label: String}> = [
+    {aos: 1000, label: 'Data Driven'},
+    {aos: 1000, label: 'y producto'},
+    {aos: 1000, label: 'digital'},
+  ]
+
+
+  firstText: string = 'Visualización de datos P2P y P2M';
+  secondText: string = 'During these uncertain times, many of us find ourselves in introspection mode, with new ideas and resolving to do better. We wish we could help everyone and fix everything. ';
+  displayImage:string = 'hidden'
 }
