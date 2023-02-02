8f841b0e727b3738d2a6a0e1d9ec335709b55562
jandro29 <ascolanr@gmail.com>
2023-02-01 19:04:53 -0500

    first commit

--- src/app/views/index/index.component.ts
+++ src/app/views/index/index.component.ts
@@ -50,12 +50,12 @@ export class IndexComponent implements AfterViewInit, OnInit {
   displayElement: boolean = true;
 
   servicesItems = [
-    {title: 'Descubrimiento de producto', text: 'Planifique y evalúe las características esenciales de su producto para lograr sus objetivos comerciales y eliminar posibles errores.', state: false, length: '2xl:w-auto', route: '/servicios'},
-    {title: 'Diseño de Productos', text: 'Crea experiencias de usuario para sus productos digitales. Resuelva problemas reales y mejore sus métricas comerciales a través de interfaces estéticas.', state: false, length: '2xl:w-[75%]', route: '/servicios'},
-    {title: 'Desarrollo de software', text: 'Implemente interfaces de usuario a medida y receptivas que sean altamente interactivas y brinden a los usuarios una experiencia similar a la nativa.', state: false, length: '2xl:w-[75%]', route: '/servicios'},
-    {title: 'Sistemas de diseño', text: 'Democratiza el diseño y desarrollo, para una experiencia de usuario superior y una mayor eficiencia en el tiempo de creación de tus productos digitales.', state: false, length: 'xl:w-[72%]', route: '/servicios'},
-    {title: 'Investigación y consultoría digital', text: 'Tome las decisiones correctas y realice investigación en sus usuarios. Entiende su comportamiento y decodifica sus intereses.', state: false, length: '2xl:w-auto', route: '/servicios'},
-    {title: 'Transformación Cultural', text: 'Adaptamos tu negocio al mundo digital desde una estrategia centrada en la experiencia de usuario que optimice el crecimiento de su negocio', state: false, length: '2xl:w-[75%]', route: '/servicios'},
+    {img:'assets/img/imgmobilindex/descubrimientodeproductosmobil.jpg',    title: 'Descubrimiento de producto', text: 'Planifique y evalúe las características esenciales de su producto para lograr sus objetivos comerciales y eliminar posibles errores.', state: false, length: '2xl:w-auto', route: '/servicios'},
+    {img:'assets/img/imgmobilindex/disenhodeproductosmobil.jpg',           title: 'Diseño de Productos', text: 'Crea experiencias de usuario para sus productos digitales. Resuelva problemas reales y mejore sus métricas comerciales a través de interfaces estéticas.', state: false, length: '2xl:w-[75%]', route: '/servicios'},
+    {img: 'assets/img/imgmobilindex/desarrollodesoftwaremobil.jpg',        title: 'Desarrollo de software', text: 'Implemente interfaces de usuario a medida y receptivas que sean altamente interactivas y brinden a los usuarios una experiencia similar a la nativa.', state: false, length: '2xl:w-[75%]', route: '/servicios'},
+    {img: 'assets/img/imgmobilindex/sitemasdedisenhomobil.jpg',            title: 'Sistemas de diseño', text: 'Democratiza el diseño y desarrollo, para una experiencia de usuario superior y una mayor eficiencia en el tiempo de creación de tus productos digitales.', state: false, length: 'xl:w-[72%]', route: '/servicios'},
+    {img: 'assets/img/imgmobilindex/investigacionyconsultariadigital.jpg', title: 'Investigación y consultoría digital', text: 'Tome las decisiones correctas y realice investigación en sus usuarios. Entiende su comportamiento y decodifica sus intereses.', state: false, length: '2xl:w-auto', route: '/servicios'},
+    {img: 'assets/img/imgmobilindex/transformacioncultural.jpg',       title: 'Transformación Cultural', text: 'Adaptamos tu negocio al mundo digital desde una estrategia centrada en la experiencia de usuario que optimice el crecimiento de su negocio', state: false, length: '2xl:w-[75%]', route: '/servicios'},
   ]
 
   serviceRoute(route: any) {
